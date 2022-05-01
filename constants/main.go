9932747e86d92043ac4ea6f3252a0b04c0352be9
wg-d <wonderdream.dev@outlook.com>
2022-05-01 09:55:45 +0900

    add

---
+++ constants/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+  "fmt"
+  "math"
+)
+
+const s string = "constant string"
+
+func main() {
+  fmt.Println(s)
+
+  const n = 500000000
+
+  const d = 3e20/n
+  fmt.Println(d)
+
+  fmt.Println(int64(d))
+
+  fmt.Println(math.Sin(n))
+}
+
+
+
