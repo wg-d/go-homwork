c61cacdda964100fb752cf0f029212f744b06a3e
wg-d <wonderdream.dev@outlook.com>
2022-05-01 09:50:05 +0900

    values && variables

---
+++ variables/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+  var a = "initial"
+  fmt.Println("a = ", a)
+
+  var b,c int = 1, 2
+  fmt.Println("b = ", b, "c = ", c)
+
+  var d = true
+  fmt.Println("d = ", d)
+
+  var e int
+  //e = 216
+  fmt.Println("e = ", e)
+
+  f := "apple"
+  fmt.Println("f = ", f)
+}
